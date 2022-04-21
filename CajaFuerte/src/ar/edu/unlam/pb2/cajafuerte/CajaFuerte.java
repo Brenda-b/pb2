2dee8e79cbcd88608fd6a6e592de6c1520319559
Brenda Soledad <breendasoledad1996@gmail.com>
2022-04-21 01:10:38 -0300

    terminar ejercicio

---
+++ CajaFuerte/src/ar/edu/unlam/pb2/cajafuerte/CajaFuerte.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.cajafuerte;
+
+public class CajaFuerte {
+	
+	private Boolean estaAbierta = true;
+	
+	public Boolean estaAbierta() {
+		return estaAbierta;
+	}
+	
+	public void cerrar() {
+		this.estaAbierta() = false;
+	}
+}
